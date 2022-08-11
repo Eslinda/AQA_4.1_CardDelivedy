e644a48bd843717b53d51d17ab07390ffb345e1d
Tatiana Lyapina <antipts@gmail.com>
2022-08-11 23:58:28 +0300

    Initial commit

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,40 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999/");
+    }
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void shouldTestPositive() {
+        String date = generateDate(4);
+        $x("//input[@placeholder='Город']").setValue("Архангельск");
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.CONTROL + "a");
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        $x("//input[@placeholder='Дата встречи']").setValue(date);
+        $x("//input[@name='name']").setValue("Петрова-Васечкина Анна");
+        $x("//input[@name='phone']").setValue("+79001112233");
+        $x("//span[@class='checkbox__text']").click();
+        $x("//*[text()='Забронировать']").click();
+        $(withText("Встреча успешно забронирована на")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+}
