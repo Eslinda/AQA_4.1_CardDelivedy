e97f9d5fa6a48d8c1f74217a3f4a17e778364b13
Tatiana Lyapina <antipts@gmail.com>
2022-08-15 00:50:25 +0300

    Add test to check list of cities and calendar

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -9,6 +9,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -35,7 +36,27 @@ public class CardDeliveryTest {
         $x("//span[@class='checkbox__text']").click();
         $x("//*[text()='Забронировать']").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $x("//div[@class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + date));
+        $x("//div[@class='notification__content']").shouldHave(text("Встреча успешно забронирована на "
+                + date));
+    }
+
+    @Test
+    void shouldChooseCityFromListAndDateFromCalendar() {
+        $x("//input[@placeholder='Город']").setValue("Ар");
+        $$x("//span[@class='menu-item__control']").findBy(text("Архангельск")).click();
+        $x("//span[@class='icon icon_size_m icon_name_calendar icon_theme_alfa-on-white']").click();
+        LocalDate meetingDate = LocalDate.now().plusDays(7);
+        if ((meetingDate.getMonthValue() > LocalDate.now().getMonthValue())) {
+            $x("//div[@class='calendar__arrow calendar__arrow_direction_right']").click();
+        }
+        $$x("//tr/td").findBy(text(String.valueOf(meetingDate.getDayOfMonth()))).click();
+        $x("//input[@name='name']").setValue("Петрова-Васечкина Анна");
+        $x("//input[@name='phone']").setValue("+79001112233");
+        $x("//span[@class='checkbox__text']").click();
+        $x("//*[text()='Забронировать']").click();
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $x("//div[@class='notification__content']").shouldHave(text("Встреча успешно забронирована на "
+                + String.valueOf(meetingDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")))));
     }
 
 }
